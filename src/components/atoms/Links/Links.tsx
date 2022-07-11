63533dbe5f733ead78128e7b1f8d34898d0e83cf
Ashok Vijayendra <ashvijay@WKMZTDF3DC4C.global.publicisgroupe.net>
2022-07-10 23:55:34 -0500

    fixing issue with childrenWidth not working

--- src/components/atoms/Links/Links.tsx
+++ src/components/atoms/Links/Links.tsx
@@ -3,12 +3,12 @@ import styles from './links.module.css';
 import Container from '../Container';
 import Typography from '../Typography';
 
-export type LinksProps = { direction?: string, items: any};
+export type LinksProps = { direction?: string, items: any, style?: any};
 
-const Links: React.FC<LinksProps> = ({direction, items}: LinksProps) => {
+const Links: React.FC<LinksProps> = ({direction, items, style}: LinksProps) => {
   if(!items && !items.lenght) return null;
   return (
-    <Container direction={direction || 'column'}>
+    <Container direction={direction || 'column'} {...style}>
       {items.map((link: any) => {
         return <a href={link.url}><Typography variant='paragraph'>{link.text}</Typography></a>;
       })}
